237342a943565204b901d0837a8b9104972e6ba4
IliaPr <ilya.prianichnikov@gmail.com>
2023-05-24 00:48:10 +0700

    task 6.4

--- catalog/views.py
+++ catalog/views.py
@@ -13,10 +13,14 @@ def home_page(request):
 def contact_page(request):
     return render(request, 'catalog/contact.html')
 
-def product(request, pk):
+def product(request, pk=None):
     product_item = Product.objects.get(pk=pk)
     context = {
         'title': product_item.name,
         'desc': product_item.description,
+        'category': product_item.category,
+        'price': product_item.price,
+        'create_date': product_item.create_date,
+        'change_date': product_item.change_date,
     }
     return render(request, 'catalog/product.html', context)

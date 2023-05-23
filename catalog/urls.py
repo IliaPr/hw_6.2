0d6c8f1d1a8183ab0d661cd70b55e53927c5da52
IliaPr <ilya.prianichnikov@gmail.com>
2023-05-23 11:04:42 +0700

    error with product card

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from catalog.views import home_page, contact_page
+from catalog.views import home_page, contact_page, product
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
@@ -7,4 +7,6 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', home_page, name='home'),
     path('contact/', contact_page, name='contact'),
+    path('home/', home_page, name='home'),
+    path('product/<int:pk>/', product, name='product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
+
 # Create your views here.
 def home_page(request):
-    return render(request, 'catalog/home.html')
+    context = {
+        'object_list': Product.objects.all()
+    }
+    return render(request, 'catalog/home.html', context)
 
 def contact_page(request):
     return render(request, 'catalog/contact.html')
+
+def product(request, pk):
+    product_item = Product.objects.get(pk=pk)
+    context = {
+        'object': product_item,
+        'title': product_item.name
+    }
+    return render(request, 'catalog/product.html', context)
